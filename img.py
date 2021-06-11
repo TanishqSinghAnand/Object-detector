431f158e1bb82b27176606631bacc5c9e2065ec5
Tanishq Singh Anand <anandtanishqs@gmail.com>
2021-06-11 21:27:16 +0530

    Added webcam integration

--- img.py
+++ img.py
@@ -1,12 +1,11 @@
 import cv2
 
-img = cv2.imread('Obama.png')
+img = cv2.imread('person.jpg')
 
 classNames = []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
     classNames= f.read().rstrip('\n').split('\n')
-print(classNames)
 
 configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 weightPath = 'frozen_inference_graph.pb'
@@ -21,5 +20,10 @@ net.setInputSwapRB(True)
 classIds, confs , bbox = net.detect(img,confThreshold=0.5)
 print(classIds,bbox )
 
+for classId , confidence, box in zip(classIds.flatten(),confs.flatten(),bbox):
+    # cv2.rectangle(img,box,color=(0,255,0))
+    print(box)
+    cv2.rectangle(img,box,color=(0,255,0),thickness=1)
+    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
 cv2.imshow("Output",img)
 cv2.waitKey(0)
